294b3e8a6ff131dd3e0f7784ec7fb5077b40d4e5
tanesia911 <109200335+tanesia911@users.noreply.github.com>
2026-03-11 23:59:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -203,5 +203,5 @@ def static_files(path):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
